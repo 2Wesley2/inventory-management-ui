6aed2e157292d2ea78de6199f04c93d746a25624
2Wesley2 <wesley.fer.almeida98@gmail.com>
2024-06-11 17:04:07 -0300

    chore: configure initial project setup with VueJS 3, Nuxt3, and Vuetify 3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,22 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify from 'vite-plugin-vuetify'
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  css: [
+    'vuetify/styles/main.sass',
+    '@mdi/font/css/materialdesignicons.css',
+  ],
+  build: {
+    transpile: ['vuetify'],
+  },
+  hooks: {
+    'vite:extendConfig': (config) => {
+      config.plugins = config.plugins || []
+      config.plugins.push(vuetify())
+    },
+  },
+  vite: {
+    define: {
+      'process.env.DEBUG': false,
+    },
+  },
 })
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col cols="12">
+            <v-card>
+              <v-card-title>Inventory Management System</v-card-title>
+              <v-card-text>
+                Bem-vindo ao Sistema de Gerenciamento de Estoque.
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+</script>
